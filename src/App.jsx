0b0cbe3ae7c4bab1b7489ccbf8483ad5e0557c81
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-24 01:31:22 +0530

    ShadCN

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,36 @@
-import React from "react";
+import { Button } from "@/components/ui/button"
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
 
-const App = () => {
+function App() {
   return (
-    <main className="min-h-screen bg-gray-100 flex flex-col items-center pt-16">
-      {/* Title */}
-      <h2 className="text-4xl font-semibold mb-8">Task Manager</h2>
 
-      {/* Card */}
-      <div className="bg-white border border-gray-300 rounded-lg p-6 w-full max-w-3xl">
-        {/* Input Row */}
-        <div className="flex gap-3 mb-6">
-          <input
-            type="text"
-            placeholder="Add task..."
-            className="flex-1 border border-gray-300 rounded px-4 py-3"
-          />
-          <button className=" cursor-pointer px-6 py-3 bg-black text-white rounded">
-            Add
-          </button>
-        </div>
+    <>
+    
+   <InputOTP maxLength={6}>
+  <InputOTPGroup>
+    <InputOTPSlot index={0} />
+    <InputOTPSlot index={1} />
+    <InputOTPSlot index={2} />
+  </InputOTPGroup>
+  
+  <InputOTPGroup>
+    <InputOTPSlot index={3} />
+    <InputOTPSlot index={4} />
+    <InputOTPSlot index={5} />
+  </InputOTPGroup>
+</InputOTP>
 
-        {/* Task List (UI only) */}
-        <div className="space-y-3">
-          <div className="flex justify-between items-center border p-3 rounded">
-            <span>Learn React</span>
-            <button className="text-red-500">Delete</button>
-          </div>
-
-          <div className="flex justify-between items-center border p-3 rounded">
-            <span>Build Task App</span>
-            <button className="text-red-500">Delete</button>
-          </div>
-
-          <div className="flex justify-between items-center border p-3 rounded">
-            <span>Practice useEffect</span>
-            <button className="text-red-500">Delete</button>
-          </div>
-        </div>
+<div className="flex min-h-svh flex-col items-center justify-center">
+      <Button className = "cursor-pointer" >Primary</Button>
       </div>
-    </main>
-  );
-};
 
-export default App;
+    </>
+  )
+}
+
+export default App
